5f65a10a3e6c14a196b2a866ac08035628638e4b
gerardo montivero <gerardo.montivero@gmail.com>
2022-03-18 11:54:14 -0300

    clase 16

--- routes/web.php
+++ routes/web.php
@@ -16,3 +16,31 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('welcome');
 })->name('main');
+
+Route::get('products', function () {
+    return 'Esta es la lista de productos' ;
+})->name('products.index');
+
+Route::get('products/create', function () {
+    return 'Este es el formulario para agregar un producto' ;
+})->name('products.create');
+
+Route::post('products', function () {
+    //
+})->name('products.store');
+
+Route::get('products/{product}', function ($product) {
+    return "Retorna un producto con id {$product}" ;
+})->name('products.show');
+
+Route::get('products/{product}/edit', function ($product) {
+    return "Un form para editar el producto {$product}" ;
+})->name('products.edit');
+
+Route::match(['put', 'patch'],'products/{product}', function ($product) {
+    //
+})->name('products.update');
+
+Route::delete('products/{product}', function ($product) {
+    //
+})->name('products.destroy');
