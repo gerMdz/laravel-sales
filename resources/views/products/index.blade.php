0c5add177fdf392a0a62defeb285a95988bc1d4b
gerardo montivero <gerardo.montivero@gmail.com>
2022-03-29 09:12:17 -0300

    clase 22, intro a las db

--- resources/views/products/index.blade.php
+++ resources/views/products/index.blade.php
@@ -7,7 +7,7 @@
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-EVSTQN3/azprG1Anm3QDgpJLIm9Nao0Yz1ztcQTwFspd3yD65VohhpuuCOmLASjC" crossorigin="anonymous">
 
-    <title>Document</title>
+    <title>Laravel desde cero</title>
 </head>
 <body>
 <h3>Lista de productos</h3>
