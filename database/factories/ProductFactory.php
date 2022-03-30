7930b63ed44f34cb6add40ec0d4643153ecc261b
gerardo montivero <gerardo.montivero@gmail.com>
2022-03-30 10:58:43 -0300

    clase 2, factory and seeders

---
+++ database/factories/ProductFactory.php
@@ -0,0 +1,28 @@
+<?php
+
+namespace Database\Factories;
+
+use App\Models\Product;
+use Illuminate\Database\Eloquent\Factories\Factory;
+
+class ProductFactory extends Factory
+{
+    protected $model = Product::class;
+
+    /**
+     * Define the model's default state.
+     *
+     * @return array
+     */
+    public function definition(): array
+    {
+        return [
+            'id' => $this->faker->uuid,
+            'title' => $this->faker->sentence(3),
+            'description' => $this->faker->paragraph(1),
+            'price' => $this->faker->randomFloat(2, 3, 100),
+            'stock' => $this->faker->numberBetween(1,10),
+            'status' => $this->faker->randomElement(['available', 'unavailable']),
+        ];
+    }
+}
--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -2,6 +2,7 @@
 
 namespace Database\Seeders;
 
+use App\Models\Product;
 use Illuminate\Database\Seeder;
 
 class DatabaseSeeder extends Seeder
@@ -13,6 +14,6 @@ class DatabaseSeeder extends Seeder
      */
     public function run()
     {
-        // \App\Models\User::factory(10)->create();
+         $product = Product::factory(50)->create();
     }
 }
