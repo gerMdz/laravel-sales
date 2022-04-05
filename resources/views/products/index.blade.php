a43c73f523f840a4ae9f39ca3bced3a0bf7885b5
gerardo montivero <gerardo.montivero@gmail.com>
2022-04-05 10:12:19 -0300

    clase 30, Blade and templates

--- resources/views/products/index.blade.php
+++ resources/views/products/index.blade.php
@@ -1,16 +1,5 @@
-<!doctype html>
-<html lang="es">
-<head>
-    <meta charset="UTF-8">
-    <meta name="viewport"
-          content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
-    <meta http-equiv="X-UA-Compatible" content="ie=edge">
-    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/css/bootstrap.min.css" rel="stylesheet"
-          integrity="sha384-EVSTQN3/azprG1Anm3QDgpJLIm9Nao0Yz1ztcQTwFspd3yD65VohhpuuCOmLASjC" crossorigin="anonymous">
-
-    <title>Laravel desde cero</title>
-</head>
-<body>
+@extends('layouts.master')
+@section('content')
 <h3>Lista de productos</h3>
         @empty($products))
         <div class="alert alert-warning">
@@ -46,5 +35,4 @@
                 </table>
             </div>
         @endempty
-</body>
-</html>
+@endsection
--- resources/views/products/show.blade.php
+++ resources/views/products/show.blade.php
@@ -1,22 +1,11 @@
-<!doctype html>
-<html lang="en">
-<head>
-    <meta charset="UTF-8">
-    <meta name="viewport"
-          content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
-    <meta http-equiv="X-UA-Compatible" content="ie=edge">
-    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-EVSTQN3/azprG1Anm3QDgpJLIm9Nao0Yz1ztcQTwFspd3yD65VohhpuuCOmLASjC" crossorigin="anonymous">
+@extends('layouts.master')
+@section('content')
+    <h3>
+        {{ $product->title }} <small>({{ $product->id  }})</small>
+    </h3>
+    <p>{{ $product->description }}</p>
+    <p>   {{ $product->precio }} </p>
+    <p>   {{ $product->stock }} </p>
+    <p>   {{ $product->status }} </p>
+@endsection
 
-    <title>Document</title>
-</head>
-<body>
-<h3>
-    {{ $product->title }} <small>({{ $product->id  }})</small>
-</h3>
-<p>{{ $product->description }}</p>
-<p>   {{ $product->precio }} </p>
-<p>   {{ $product->stock }} </p>
-<p>   {{ $product->status }} </p>
-
-</body>
-</html>
