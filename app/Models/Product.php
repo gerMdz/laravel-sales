d61be07619e9dc4cf8a6a10c42a71a7dd701c500
gerardo montivero <gerardo.montivero@gmail.com>
2022-05-10 09:04:59 -0300

    Usando y creando ManyToMany

--- app/Models/Product.php
+++ app/Models/Product.php
@@ -4,6 +4,7 @@ namespace App\Models;
 
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Relations\BelongsToMany;
 
 class Product extends Model
 {
@@ -16,5 +17,15 @@ class Product extends Model
         'stock',
         'status',
     ];
+
+    public function carts(): BelongsToMany
+    {
+        return $this->belongsToMany(Cart::class)->withPivot('quantity');
+    }
+
+    public function orders(): BelongsToMany
+    {
+        return $this->belongsToMany(Order::class)->withPivot('quantity');
+    }
 
 }
