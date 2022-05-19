d51733a5ac3e3abfe8a7a052784c4f194f6ae908
gerardo montivero <gerardo.montivero@gmail.com>
2022-05-19 08:22:56 -0300

    Seeder de relaciones simples

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -2,7 +2,10 @@
 
 namespace Database\Seeders;
 
+use App\Models\Order;
+use App\Models\Payment;
 use App\Models\Product;
+use App\Models\User;
 use Illuminate\Database\Seeder;
 
 class DatabaseSeeder extends Seeder
@@ -14,6 +17,23 @@ class DatabaseSeeder extends Seeder
      */
     public function run()
     {
-         $product = Product::factory(50)->create();
+        $product = Product::factory(50)->create();
+
+        $users = User::factory(20)->create();
+
+        $orders = Order::factory(10)
+            ->make()
+            ->each(function ($order) use ($users) {
+                $order->customer_id = $users->random()->id;
+                $order->save();
+
+                $payment = Payment::factory()->make();
+//             $payment->order_id = $order->id;
+//             $payment->save();
+
+                $order->payment()->save($payment);
+            });
+
+
     }
 }
