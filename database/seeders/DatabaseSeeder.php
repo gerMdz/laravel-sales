03662ff2ae3664bdf5c989e5d3085d1de10b48d1
gerardo montivero <gerardo.montivero@gmail.com>
2022-05-24 07:02:40 -0300

    Seeder de relaciones polimórficas

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -2,6 +2,8 @@
 
 namespace Database\Seeders;
 
+use App\Models\Cart;
+use App\Models\Image;
 use App\Models\Order;
 use App\Models\Payment;
 use App\Models\Product;
@@ -17,9 +19,16 @@ class DatabaseSeeder extends Seeder
      */
     public function run()
     {
-        $product = Product::factory(50)->create();
 
-        $users = User::factory(20)->create();
+
+        $users = User::factory(20)
+            ->create()
+            ->each(function ($user) {
+                $image = Image::factory()
+                    ->user()
+                    ->make();
+                $user->image()->save($image);
+            });
 
         $orders = Order::factory(10)
             ->make()
@@ -33,6 +42,26 @@ class DatabaseSeeder extends Seeder
 
                 $order->payment()->save($payment);
             });
+
+        $carts = Cart::factory(20)->create();
+
+        $products = Product::factory(50)
+            ->create()
+            ->each(function ($product) use ($orders, $carts){
+                $order = $orders->random();
+                $order->products()->attach([
+                    $product->id => ['quantity' => mt_rand(1,3)]
+                ]);
+
+                $cart = $carts->random();
+                $cart->products()->attach([
+                    $product->id => ['quantity' => mt_rand(1,3)]
+                ]);
+
+                $images = Image::factory(mt_rand(1,4))->make();
+                $product->images()->saveMany($images);
+            });
+
 
 
     }
